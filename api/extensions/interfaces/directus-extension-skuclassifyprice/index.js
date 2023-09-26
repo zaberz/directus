cb5d914fb085b3221d1bd44e18a6e096d9451121
zhangjie <274588107@qq.com>
2023-09-26 17:51:03 +0800

    use tencent

---
+++ api/extensions/interfaces/directus-extension-skuclassifyprice/index.js
@@ -0,0 +1,10 @@
+import SkuClassifyPrice from './SkuClassifyPrice.vue'
+export default {
+  id: 'skuclassifyprice',
+  name: 'sku品类价格',
+  icon: 'box',
+  description: '获取系统录入的sku品类价格',
+  component: SkuClassifyPrice,
+  options: null,
+  types: ['float'],
+};
