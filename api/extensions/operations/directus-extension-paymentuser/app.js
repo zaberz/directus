2d4c8b91b4a20451d9a705b403be860554f3e9a9
zhangjie <274588107@qq.com>
2023-09-28 15:02:34 +0800

    add operations

---
+++ api/extensions/operations/directus-extension-paymentuser/app.js
@@ -0,0 +1,22 @@
+export default {
+  id: 'paymentuser',
+  name: '更新付款顾问',
+  icon: 'box',
+  description: '更新付款的顾问，统计使用',
+  overview: ({text}) => {
+    return [
+      // {label: 'Text', text}
+    ]
+  },
+  options: [
+    // {
+    // field: 'text',
+    // name: 'Text',
+    // type: 'string',
+    // meta: {
+    //   width: 'full',
+    //   interface: 'input',
+    // },
+  // },
+]
+}
