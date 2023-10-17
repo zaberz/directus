6f8231c3812df7589d22b2b8e59bf51673b58d44
zhangjie <274588107@qq.com>
2023-10-17 15:29:44 +0800

    add operations

---
+++ extensions/directus-extension-orderprint/src/index.js
@@ -0,0 +1,13 @@
+import ModuleComponent from './module.vue';
+
+export default {
+	id: 'orderPrint',
+	name: 'orderPrint',
+	icon: 'box',
+	routes: [
+		{
+			path: '',
+			component: ModuleComponent,
+		},
+	],
+};
